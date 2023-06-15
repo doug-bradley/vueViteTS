8230f826a409a2a3ed4c488b44dfb08bd4c27eb6
Tyrone Stafford <undyingdesign@gmail.com>
2023-06-15 17:28:08 +0200

    Tyrone commits initial progress

---
+++ src/components/Grid/RowEditButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <div ref="editButton">
+    <span class="text-teal cursor-pointer" @click="show = !show">
+        <svg viewBox="0 0 17 17" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 hover:text-blue-400">
+            <path d="M5.79876 14.1468L1 15.5152L2.37177 10.728L11.6251 1.49686C12.2892 0.834381 13.3604 0.834381 14.0245 1.49686L15.0521 2.522C15.7162 3.18448 15.7162 4.25314 15.0521 4.91562L5.79876 14.1468Z" stroke-linejoin="round"/>
+            <path d="M13.6789 6.27911L10.252 2.86035" stroke-linejoin="round"/>
+        </svg>
+
+
+    </span>
+    <Popup :anchor="'editButton'" :show="show" :popup-class="'popup-content'">
+        <ColumnFilterView :invoiceColumns="props.invoiceColumns" />
+    </Popup>
+    </div>
+</template>
+
+<script setup lang="ts">
+// import { Button } from '@progress/kendo-vue-buttons';
+import { GridColumnProps } from '@progress/kendo-vue-grid';
+import { Popup } from '@progress/kendo-vue-popup';
+import ColumnFilterView from './ColumnFilterView.vue';
+
+import { ref } from 'vue';
+
+interface IProps {
+    invoiceColumns: GridColumnProps[];
+}
+
+const props = defineProps<IProps>();
+const show = ref(false);
+
+</script>
+
+<style scoped>
+:deep(.popup-content.k-popup.k-child-animation-container) {
+    border-radius: 8px;
+}
+</style>
---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="flex flex-col w-full h-screen overflow-y-scroll">
+        <Nav></Nav>
+        <div class="border flex flex-row h-full">
+            <div class="bg-primary h-full">
+                <div class="w-[194px]">
+                    <!-- custom dropdown button -->
+                    <div class="btn-dropdown">
+                        <div class="flex flex-row items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-[8px]">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            New request
+                        </div>
+                        <div class="flex flex-col justify-center ml-2 pl-3 border-l-[1px] border-white border-opacity-50">
+                            <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9 1L5 5L1 1" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-10">
+                <slot></slot> <!-- WorkcenterGrid goes in here -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Nav from '../Nav/Nav.vue';
+
+</script>
+
+<style scoped>
+.btn-dropdown{
+    @apply flex flex-row justify-between mx-auto bg-secondary text-white rounded-md px-2 w-[159px] h-[40px] mt-8 text-[13px] cursor-pointer shadow-none transition-shadow hover:shadow-lg;
+}
+</style>
